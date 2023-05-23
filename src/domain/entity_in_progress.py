340cd33a32a10bcd09968f98c7d98233b46b6a40
rydnr <github@acm-sl.org>
2023-05-23 10:50:56 +0200

    Some bug fixes

--- src/domain/entity_in_progress.py
+++ src/domain/entity_in_progress.py
@@ -8,7 +8,7 @@ class EntityInProgress(Entity):
     Represents an Entity which doesn't have all information yet.
     """
 
-    _pending = []
+    _pending = {}
 
     def __init__(self):
         """Creates a new EntityInProgress instance"""
@@ -22,18 +22,19 @@ class EntityInProgress(Entity):
 
     @classmethod
     def matching(cls, **kwargs):
-        return FlakeInProgress._pending.get(cls.build_key_from_attributes(kwargs), None)
+        return cls._pending.get(cls.build_key_from_attributes(**kwargs), None)
 
     @classmethod
     def build_key_from_attributes(cls, **kwargs) -> str:
         """Builds a key"""
-        for key in self.__class__.primary_key():
+        items = []
+        for key in cls.primary_key():
             items.append(f'"{key}": "{kwargs.items().get(key, "")}"')
         return f'{{ {", ".join(items)} }}'
 
     @classmethod
     def build_key_from_entity(cls, entityInProgress) -> str:
         """Builds a key"""
-        for key in self.__class__.primary_key():
+        for key in cls.primary_key():
             items.append(f'"{key}": "{getattr(entityInProgress, key, "")}"')
         return f'{{ {", ".join(items)} }}'
